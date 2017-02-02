3adca25c71b63c3b2194d1be5238f52fa0036586
Tayler How <taylerhow@gmail.com>
2017-02-01 20:56:25 -0500

    Implemented DefaultServletManager and DefaultServlet

--- src/main/java/servlet/DefaultServlet.java
+++ src/main/java/servlet/DefaultServlet.java
@@ -1,5 +1,52 @@
 package servlet;
 
-public class DefaultServlet {
+import protocol.HttpRequest;
+import protocol.HttpResponseBuilder;
+
+public class DefaultServlet extends AHttpServlet {
+
+	public DefaultServlet(String resourcePath) {
+		super(resourcePath);
+	}
+
+	@Override
+	public void init() {
+		// Nothing to do here
+	}
+
+	@Override
+	public void destroy() {
+		// Nothing to do here
+	}
+
+	@Override
+	public void doGet(HttpRequest request, HttpResponseBuilder responseBuilder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doHead(HttpRequest request, HttpResponseBuilder responseBuilder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doPost(HttpRequest request, HttpResponseBuilder responseBuilder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doPut(HttpRequest request, HttpResponseBuilder responseBuilder) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doDelete(HttpRequest request, HttpResponseBuilder responseBuilder) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
