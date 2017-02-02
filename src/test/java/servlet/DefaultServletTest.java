58fb80d09786fffbc4deb48fd82a474f29bc511d
Tayler How <taylerhow@gmail.com>
2017-02-02 17:47:05 -0500

    Finished unit tests

--- src/test/java/servlet/DefaultServletTest.java
+++ src/test/java/servlet/DefaultServletTest.java
@@ -6,8 +6,11 @@ import static org.mockito.Mockito.when;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
+import java.util.HashMap;
 
+import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -15,6 +18,8 @@ import org.mockito.Mockito;
 import protocol.HttpRequest;
 import protocol.HttpResponse;
 import protocol.HttpResponseBuilder;
+import protocol.Keywords;
+import protocol.Protocol;
 
 public class DefaultServletTest {
 	private static AHttpServlet defaultServlet;
@@ -34,6 +39,12 @@ public class DefaultServletTest {
 		newFileName = "new.txt";
 		rootDirectory = "src/test/resources";
 
+		// Initialize default servlet
+		defaultServlet = new DefaultServlet(rootDirectory);
+	}
+
+	@Before
+	public void setUpBeforeEachTest() throws Exception {
 		// Initialize test file
 		File testFile = new File(rootDirectory, defaultFileName);
 
@@ -42,9 +53,6 @@ public class DefaultServletTest {
 
 		writer.close();
 
-		// Initialize test directory
-		File testDirectory = new File(rootDirectory, directoryName);
-
 		// Initialize nested test file
 		File nestedFile = new File(rootDirectory, directoryName + "/" + nestedFileName);
 
@@ -52,9 +60,15 @@ public class DefaultServletTest {
 		nestedWriter.write(nestedFileContent);
 
 		nestedWriter.close();
-
-		// Initialize default servlet
-		defaultServlet = new DefaultServlet(rootDirectory);
+	}
+	
+	@After
+	public void tearDownAfterEachTwst() throws Exception {
+		// Delete new file
+		File newFile = new File(rootDirectory, newFileName);
+		if (newFile.exists()) {
+			newFile.delete();
+		}
 	}
 
 	@Test
@@ -94,7 +108,7 @@ public class DefaultServletTest {
 	}
 
 	@Test
-	public void testGet200OK() throws Exception {
+	public void testGet200Ok() throws Exception {
 		HttpRequest request = Mockito.mock(HttpRequest.class);
 		when(request.getMethod()).thenReturn("GET");
 		when(request.getVersion()).thenReturn("HTTP/1.1");
@@ -110,7 +124,7 @@ public class DefaultServletTest {
 		int actual = response.getStatus();
 		assertEquals(expected, actual);
 
-		String expectedBody = "This is the initialization content in index.html!";
+		String expectedBody = defaultFileContent;
 
 		FileInputStream fis = new FileInputStream(response.getFile());
 		byte[] data = new byte[(int) response.getFile().length()];
@@ -120,14 +134,13 @@ public class DefaultServletTest {
 
 		assertEquals(expectedBody, actualBody);
 	}
-	
+
 	@Test
-	public void testGet200OKNestedFile() throws Exception {
+	public void testGet200OkNestedFile() throws Exception {
 		HttpRequest request = Mockito.mock(HttpRequest.class);
 		when(request.getMethod()).thenReturn("GET");
 		when(request.getVersion()).thenReturn("HTTP/1.1");
 		when(request.getUri()).thenReturn("/" + directoryName + "/" + nestedFileName);
-		
 
 		HttpResponseBuilder rb = new HttpResponseBuilder();
 
@@ -139,7 +152,7 @@ public class DefaultServletTest {
 		int actual = response.getStatus();
 		assertEquals(expected, actual);
 
-		String expectedBody = "This is the initialization content in test.txt!";
+		String expectedBody = nestedFileContent;
 
 		FileInputStream fis = new FileInputStream(response.getFile());
 		byte[] data = new byte[(int) response.getFile().length()];
@@ -149,9 +162,9 @@ public class DefaultServletTest {
 
 		assertEquals(expectedBody, actualBody);
 	}
-	
+
 	@Test
-	public void testGet200OKDefaultUri() throws Exception {
+	public void testGet200OkDefaultUri() throws Exception {
 		HttpRequest request = Mockito.mock(HttpRequest.class);
 		when(request.getMethod()).thenReturn("GET");
 		when(request.getVersion()).thenReturn("HTTP/1.1");
@@ -167,7 +180,7 @@ public class DefaultServletTest {
 		int actual = response.getStatus();
 		assertEquals(expected, actual);
 
-		String expectedBody = "This is the initialization content in index.html!";
+		String expectedBody = defaultFileContent;
 
 		FileInputStream fis = new FileInputStream(response.getFile());
 		byte[] data = new byte[(int) response.getFile().length()];
@@ -177,7 +190,7 @@ public class DefaultServletTest {
 
 		assertEquals(expectedBody, actualBody);
 	}
-	
+
 	@Test
 	public void testHead404NotFound() {
 		HttpRequest request = Mockito.mock(HttpRequest.class);
@@ -215,7 +228,7 @@ public class DefaultServletTest {
 	}
 
 	@Test
-	public void testHead200OK() throws Exception {
+	public void testHead200Ok() throws Exception {
 		HttpRequest request = Mockito.mock(HttpRequest.class);
 		when(request.getMethod()).thenReturn("HEAD");
 		when(request.getVersion()).thenReturn("HTTP/1.1");
@@ -230,19 +243,19 @@ public class DefaultServletTest {
 		int expected = 200;
 		int actual = response.getStatus();
 		assertEquals(expected, actual);
-		
+
 		int expectedLength = defaultFileContent.length();
 		int actualLength = Integer.parseInt(response.getHeader().get("Content-Length"));
 		assertEquals(expectedLength, actualLength);
 	}
-	
+
 	@Test
-	public void testHead200OKNestedFile() throws Exception {
+	public void testHead200OkNestedFile() throws Exception {
 		HttpRequest request = Mockito.mock(HttpRequest.class);
 		when(request.getMethod()).thenReturn("HEAD");
 		when(request.getVersion()).thenReturn("HTTP/1.1");
 		when(request.getUri()).thenReturn("/" + directoryName + "/" + nestedFileName);
-		
+
 		HttpResponseBuilder rb = new HttpResponseBuilder();
 
 		defaultServlet.doHead(request, rb);
@@ -252,14 +265,14 @@ public class DefaultServletTest {
 		int expected = 200;
 		int actual = response.getStatus();
 		assertEquals(expected, actual);
-		
+
 		int expectedLength = nestedFileContent.length();
 		int actualLength = Integer.parseInt(response.getHeader().get("Content-Length"));
 		assertEquals(expectedLength, actualLength);
 	}
-	
+
 	@Test
-	public void testHead200OKDefaultUri() throws Exception {
+	public void testHead200OkDefaultUri() throws Exception {
 		HttpRequest request = Mockito.mock(HttpRequest.class);
 		when(request.getMethod()).thenReturn("HEAD");
 		when(request.getVersion()).thenReturn("HTTP/1.1");
@@ -274,18 +287,326 @@ public class DefaultServletTest {
 		int expected = 200;
 		int actual = response.getStatus();
 		assertEquals(expected, actual);
-		
+
 		int expectedLength = defaultFileContent.length();
 		int actualLength = Integer.parseInt(response.getHeader().get("Content-Length"));
 		assertEquals(expectedLength, actualLength);
 	}
 
+	@Test
+	public void testPost400BadRequestIsDirectory() throws Exception {
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("POST");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/" + directoryName);
+
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doPost(request, rb);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 400;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testPost200OkAppendToFile() throws Exception {
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("POST");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/" + defaultFileName);
+		String postContent = "POST Content";
+		when(request.getBody()).thenReturn(postContent.toCharArray());
+		HashMap<String, String> fakeRequestHeaders = new HashMap<String, String>();
+		fakeRequestHeaders.put(Protocol.getProtocol().getStringRep(Keywords.CONTENT_LENGTH),
+				String.valueOf(postContent.length()));
+		when(request.getHeader()).thenReturn(fakeRequestHeaders);
+
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doPost(request, rb);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 200;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+
+		String expectedBody = defaultFileContent + postContent;
+
+		FileInputStream fis = new FileInputStream(response.getFile());
+		byte[] data = new byte[(int) response.getFile().length()];
+		fis.read(data);
+		fis.close();
+		String actualBody = new String(data, "UTF-8");
+
+		assertEquals(expectedBody, actualBody);
+	}
+	
+	@Test
+	public void testPost200OkCreateNewFile() throws Exception {
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("POST");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/" + newFileName);
+		String postContent = "POST Content";
+		when(request.getBody()).thenReturn(postContent.toCharArray());
+		HashMap<String, String> fakeRequestHeaders = new HashMap<String, String>();
+		fakeRequestHeaders.put(Protocol.getProtocol().getStringRep(Keywords.CONTENT_LENGTH),
+				String.valueOf(postContent.length()));
+		when(request.getHeader()).thenReturn(fakeRequestHeaders);
+
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doPost(request, rb);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 200;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+
+		String expectedBody = postContent;
+
+		FileInputStream fis = new FileInputStream(response.getFile());
+		byte[] data = new byte[(int) response.getFile().length()];
+		fis.read(data);
+		fis.close();
+		String actualBody = new String(data, "UTF-8");
+
+		assertEquals(expectedBody, actualBody);
+	}
+	
+	@Test
+	public void testPost500InternalServerErrorFileLocked() throws Exception {
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("POST");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/" + defaultFileName);
+		
+		File file = new File(rootDirectory, defaultFileName);
+		file.setWritable(false);
+
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doPost(request, rb);
+		
+		file.setWritable(true);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 500;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testPut400BadRequestIsDirectory() throws Exception {
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("PUT");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/" + directoryName);
+
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doPut(request, rb);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 400;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testPost200OkOverwriteFile() throws Exception {
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("PUT");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/" + defaultFileName);
+		String putContent = "POST Content";
+		when(request.getBody()).thenReturn(putContent.toCharArray());
+		HashMap<String, String> fakeRequestHeaders = new HashMap<String, String>();
+		fakeRequestHeaders.put(Protocol.getProtocol().getStringRep(Keywords.CONTENT_LENGTH),
+				String.valueOf(putContent.length()));
+		when(request.getHeader()).thenReturn(fakeRequestHeaders);
+
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doPut(request, rb);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 200;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+
+		String expectedBody = putContent;
+
+		FileInputStream fis = new FileInputStream(response.getFile());
+		byte[] data = new byte[(int) response.getFile().length()];
+		fis.read(data);
+		fis.close();
+		String actualBody = new String(data, "UTF-8");
+
+		assertEquals(expectedBody, actualBody);
+	}
+	
+	@Test
+	public void testPut200OkCreateNewFile() throws Exception {
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("PUT");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/" + newFileName);
+		String putContent = "POST Content";
+		when(request.getBody()).thenReturn(putContent.toCharArray());
+		HashMap<String, String> fakeRequestHeaders = new HashMap<String, String>();
+		fakeRequestHeaders.put(Protocol.getProtocol().getStringRep(Keywords.CONTENT_LENGTH),
+				String.valueOf(putContent.length()));
+		when(request.getHeader()).thenReturn(fakeRequestHeaders);
+
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doPut(request, rb);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 200;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+
+		String expectedBody = putContent;
+
+		FileInputStream fis = new FileInputStream(response.getFile());
+		byte[] data = new byte[(int) response.getFile().length()];
+		fis.read(data);
+		fis.close();
+		String actualBody = new String(data, "UTF-8");
+
+		assertEquals(expectedBody, actualBody);
+	}
+	
+	@Test
+	public void testPut500InternalServerErrorFileLocked() throws Exception {
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("PUT");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/" + defaultFileName);
+		
+		File file = new File(rootDirectory, defaultFileName);
+		file.setWritable(false);
+
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doPut(request, rb);
+		
+		file.setWritable(true);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 500;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testDelete204NoContentFileDeleted() throws Exception {
+		File newFile = new File(rootDirectory, newFileName);
+		if (!newFile.exists()) {
+			newFile.createNewFile();
+		}
+		
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("DELETE");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/" + newFileName);
+		
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doDelete(request, rb);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 204;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+		
+		HttpRequest getRequest = Mockito.mock(HttpRequest.class);
+		when(getRequest.getMethod()).thenReturn("GET");
+		when(getRequest.getVersion()).thenReturn("HTTP/1.1");
+		when(getRequest.getUri()).thenReturn("/" + newFileName);
+
+		HttpResponseBuilder getrb = new HttpResponseBuilder();
+
+		defaultServlet.doGet(getRequest, getrb);
+
+		HttpResponse getResponse = getrb.generateResponse();
+
+		int getExpected = 404;
+		int getActual = getResponse.getStatus();
+		assertEquals(getExpected, getActual);
+	}
+	
+	@Test
+	public void testDelete204NoContentDirectoryDeleted() throws Exception {
+		File newFile = new File(rootDirectory, newFileName);
+		if (!newFile.exists()) {
+			newFile.mkdir();
+		}
+		
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("DELETE");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/" + newFileName);
+		
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doDelete(request, rb);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 204;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+		
+		HttpRequest getRequest = Mockito.mock(HttpRequest.class);
+		when(getRequest.getMethod()).thenReturn("GET");
+		when(getRequest.getVersion()).thenReturn("HTTP/1.1");
+		when(getRequest.getUri()).thenReturn("/" + newFileName);
+
+		HttpResponseBuilder getrb = new HttpResponseBuilder();
+
+		defaultServlet.doGet(getRequest, getrb);
+
+		HttpResponse getResponse = getrb.generateResponse();
+
+		int getExpected = 404;
+		int getActual = getResponse.getStatus();
+		assertEquals(getExpected, getActual);
+	}
+	
+	@Test
+	public void testDelete404NotFound() {
+		HttpRequest request = Mockito.mock(HttpRequest.class);
+		when(request.getMethod()).thenReturn("DELETE");
+		when(request.getVersion()).thenReturn("HTTP/1.1");
+		when(request.getUri()).thenReturn("/notFound.txt");
+
+		HttpResponseBuilder rb = new HttpResponseBuilder();
+
+		defaultServlet.doDelete(request, rb);
+
+		HttpResponse response = rb.generateResponse();
+
+		int expected = 404;
+		int actual = response.getStatus();
+		assertEquals(expected, actual);
+	}
+
 	@AfterClass
 	public static void tearDownAfterClass() {
 		// nothing to do here
-		File newFile = new File(newFileName);
-		if (newFile.exists()) {
-			newFile.delete();
-		}
 	}
 }
